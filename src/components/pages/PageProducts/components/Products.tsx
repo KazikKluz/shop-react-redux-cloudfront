b6ce2ef0d9c6735192a253f2087a8c64024f39c7
Kazik Kluz <kazikkluz@gmail.com>
2022-06-26 17:12:40 +0100

    task 4 completed

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -1,17 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
-import {Product} from "models/Product";
-import {formatAsPrice} from "utils/utils";
-import AddProductToCart from "components/AddProductToCart/AddProductToCart";
-// import axios from 'axios';
-// import API_PATHS from "constants/apiPaths";
-import productList from "./productList.json";
+import { makeStyles } from '@material-ui/core/styles';
+import { Product } from 'models/Product';
+import { formatAsPrice } from 'utils/utils';
+import AddProductToCart from 'components/AddProductToCart/AddProductToCart';
+import axios from 'axios';
+import API_PATHS from 'constants/apiPaths';
+import productList from './productList.json';
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -38,8 +38,9 @@ export default function Products() {
   useEffect(() => {
     // axios.get(`${API_PATHS.bff}/product/available/`)
     //   .then(res => setProducts(res.data));
+    axios.get(`${API_PATHS.products}`).then((res) => setProducts(res.data));
     setProducts(productList);
-  }, [])
+  }, []);
 
   return (
     <Grid container spacing={4}>
@@ -49,18 +50,16 @@ export default function Products() {
             <CardMedia
               className={classes.cardMedia}
               image={`https://source.unsplash.com/random?sig=${index}`}
-              title="Image title"
+              title='Image title'
             />
             <CardContent className={classes.cardContent}>
-              <Typography gutterBottom variant="h5" component="h2">
+              <Typography gutterBottom variant='h5' component='h2'>
                 {product.title}
               </Typography>
-              <Typography>
-                {formatAsPrice(product.price)}
-              </Typography>
+              <Typography>{formatAsPrice(product.price)}</Typography>
             </CardContent>
             <CardActions>
-              <AddProductToCart product={product}/>
+              <AddProductToCart product={product} />
             </CardActions>
           </Card>
         </Grid>
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,11 @@
-
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  products:
+    'https://mf8eqk4xtj.execute-api.eu-west-1.amazonaws.com/dev/products',
 };
 
 export default API_PATHS;
