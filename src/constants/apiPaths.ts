175dc2214b63be9618d5ade8e5db64406d2a60f6
Kazik Kluz <kazikkluz@gmail.com>
2022-07-31 14:17:00 +0100

    changed cart url pointing to the eb cart-api on aws

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://tps7udgqdj.execute-api.eu-west-1.amazonaws.com/dev/import',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://kazikkluz-cart-api-devel.eu-west-1.elasticbeanstalk.com/api',
   products:
     'https://mf8eqk4xtj.execute-api.eu-west-1.amazonaws.com/dev/products',
 };
